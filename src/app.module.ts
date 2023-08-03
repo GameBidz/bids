f11c25f2b7a5e9488d7d4eff8a959510232f2e19
jusruizsa <sruizs.jr@gmail.com>
2023-08-02 21:35:32 -0500

    add database typeorm config and define bids as module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bid } from './entities/bids';
+import { BidsModule } from './bids/bids.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'gamebidz',
+      synchronize: true,
+      logging: false,
+      entities: [Bid],
+    }),
+    BidsModule,
+  ],
 })
 export class AppModule {}
